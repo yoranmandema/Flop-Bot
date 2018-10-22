3abb5d86d00f14e97059b7cecdeae48b4d93430d
Blieper <yoranmandema@eml.cc>
2018-10-22 22:13:52 +0200

    add postgres

--- bot.js
+++ bot.js
@@ -1,16 +1,37 @@
 app = {};
+app.pg = {};
 
-app.discord     = require('discord.js');
-app.client      = new app.discord.Client();
+const pg = require('pg')
+app.discord = require('discord.js')
+app.client = new app.discord.Client()
+
+const connectionString = process.env.DATABASE_URL
+
+app.pg.pool = new pg.Pool({
+    connectionString: connectionString
+})
+
+app.pg.pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    app.pg.pool.end()
+})
+
+app.pg.client = new pg.Client({
+    connectionString: connectionString
+})
+app.pg.client.connect()
+
+app.pg.client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    app.pg.client.end()
+})
 
 app.client.on('ready', () => {
-    console.log('Bot ready!');
+    console.log('Bot ready!')
 });
 
 app.client.on('message', message => {
-    message.reply('FUCK BOI');
+    message.reply('FUCK BOI')
 });
-
-console.log(JSON.stringify(process.env));
 
 app.client.login(process.env.BOT_TOKEN);
