07fc379bb683cf5fcae783e65fb50992838b3c41
Blieper <yoranmandema@eml.cc>
2018-10-23 19:24:14 +0200

    something that kinda works

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 
-const postgresURL = process.env.DATABASE_URL
+process.env = {
+    DATABASE_URL: 'postgres://rdxnsbbbgvqnpa:a58f85d49f2166af2a785e68b68df8847c48c0adf3ad09e27620df8126da60de@ec2-54-217-236-201.eu-west-1.compute.amazonaws.com:5432/d78r5tm7lpdk2r',
+    BOT_TOKEN: 'NTA0MDEyMjYwNzM0NDAyNTcx.Dq_KRw.XDhQeaalpdmQyDMVTxXXMo8oLKo'
+}
 
 app = {}
 app.pg = {}
@@ -7,8 +10,14 @@ app.discord = {}
 
 app.modules = {
     postgres: require('pg'),
-    discord: require('discord.js')
+    discord: require('discord.js'),
+    training: require('./training/training'),
+    debug: require('debug')
 }
+
+const postgresURL = process.env.DATABASE_URL
+app.discussionChannel = null; 
+app.robotChannel = null;
 
 app.discord.client = new app.modules.discord.Client()
 
@@ -21,23 +30,81 @@ app.pg.pool = new app.modules.postgres.Pool({
 app.pg.client = new app.modules.postgres.Client({
     connectionString: postgresURL
 })
-app.pg.client.connect()
+//app.pg.client.connect()
+
+app.discord.client.login(process.env.BOT_TOKEN)
 
 app.discord.client.on('ready', () => {
     console.log('Bot ready!')
+
+    app.discussionChannel = app.discord.client.channels.find(x => x.id == '504032866829598731')
+    app.robotChannel = app.discord.client.channels.find(x => x.id == "504335196418736148")
+
+    app.modules.training.init(app)
 });
 
 app.discord.client.on('message', msg => {
+    if(msg.author.id === app.discord.client.user.id) return;
 
-    if (msg.channel.id != '504032866829598731') {
+    app.modules.training.trainOnMessage(msg.content)
+
+    if (!(msg.channel == app.discussionChannel || msg.channel == app.robotChannel)) {
         return
     }
 
-    if (msg.content.startsWith('.flop')) {
-        let message = msg.content.split('.flop')[0] || ''
+    let msgWords = msg.content.split(' ')
+    let randomWord = msgWords[Math.floor(Math.random() * msgWords.length)]
 
-        message.reply(message)
+    if (Math.random() > 0.25 && app.modules.training.words[randomWord] == undefined) {
+        return
     }
+
+    let message = generateMessage(100, randomWord)
+
+    msg.channel.sendMessage(message)
 });
 
-app.discord.client.login(process.env.BOT_TOKEN)
+var randomProperty = function (obj) {
+    var keys = Object.keys(obj)
+    var key = keys[ keys.length * Math.random() << 0]
+
+    return {key: key, value: obj[key]}
+};
+
+function generateMessage (length, start) {
+    let words = app.modules.training.words
+
+    let word;
+
+    if (words[start] != undefined) {
+        word = {key: start, value: words[start]}
+    } else {
+        word = randomProperty(words)
+        let tries = 0
+    
+        while ((word.value == undefined || word.value.length == 0) && tries < 25) {
+            word = randomProperty(words)
+            tries++;
+        }        
+    }
+
+    let message = word.key;
+
+    for (let i = 0; i < length; i++) {
+        if (word.value == undefined || word.value.length == 0) {
+            break;
+        }
+
+        let nextWord = word.value[Math.floor(Math.random() * word.value.length)];
+
+        word = {key: nextWord, value: words[nextWord]}
+
+        if (word === undefined) {
+            break;
+        }
+
+        message += " " + nextWord
+    }
+
+    return message
+}
---
+++ training/training.js
@@ -0,0 +1,100 @@
+
+let allMessages
+let discord
+let postgres
+let debug 
+let textChannels
+let words = {}
+
+let wordObject = function (w){
+    this.word = w
+    this.linked = []
+}
+
+exports.init = function (app) {
+    discord = app.discord
+    postgres = app.modules.postgres
+    debug = app.modules.debug
+
+    allMessages = new app.modules.discord.Collection()
+
+    textChannels = discord.client.channels.filter(x => x.type == "text")
+ 
+    getMessages()
+}
+
+exports.words = words;
+
+let currentChannel = 0
+
+function getMessages () {
+    app.discussionChannel.sendMessage(`Getting messages from ${textChannels.size} channels`);
+
+    textChannels = textChannels.filter(x => x != app.discussionChannel)
+    textChannels = textChannels.filter(x => x != app.robotChannel)
+
+    textChannels.tap(channel => {
+        getAllMessagesInChannel (channel.lastMessageID, channel)
+    })        
+}
+
+function getAllMessagesInChannel (before, channel) {
+    console.log(`Getting rest messages from ${channel.name}, before ${before}`)
+
+    channel.fetchMessages({ before: before, limit: 100 })
+        .then(messages => {
+            //console.log(`Received ${messages.size} messages from ${channel.name}`)
+            allMessages = allMessages.concat(messages)
+
+            if (messages.size == 100 && allMessages.size < 1500) {
+                getAllMessagesInChannel(messages.first().id, channel)
+            } else {
+                currentChannel++
+
+                app.discussionChannel.sendMessage(`Finished with channel: ${channel.name}, processed ${currentChannel}`)
+
+                if (currentChannel == textChannels.size) {
+                    //console.log(`recieved all messages! Total ${allMessages.size}`)
+
+                    app.discussionChannel.sendMessage(`recieved all messages! Total ${allMessages.size}`);
+                    onHasAllMessages()
+                }
+            }
+        }).catch(_ => {
+            currentChannel++
+            app.discussionChannel.sendMessage(`Finished with channel: ${channel.name}`)
+        })
+}
+
+function onHasAllMessages () {
+    console.log(`recieved all messages! Total ${allMessages.size}`)
+
+    let content = ''
+
+    allMessages.tap(message => {
+        content += message.content + " "
+    })
+
+    trainOnMessage(content)
+
+    console.log(words.length)
+
+    console.log(`Bot trained!`)
+}
+
+function trainOnMessage (message) {
+    let wordsInMessage = message.split(' ')
+
+    for (let i = 0; i < wordsInMessage.length - 1; i++) {
+        let currentWord = wordsInMessage[i]
+        let nextWord = wordsInMessage[i + 1]
+
+        if (words[currentWord] === undefined) {
+            words[currentWord] = []
+        } else {
+            words[currentWord].push(nextWord)
+        }
+    }
+}
+
+exports.trainOnMessage = trainOnMessage
